46e739c42ce8aa9f0c95b64942aabc5d12d89d33
Sajmir Doko <sajdoko@gmail.com>
2021-01-24 10:32:20 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# javafx-employee-holidays" 
+# javafx-employee-holidays
+
+`An employee has 20 days of holidays per year. The employee ask for holidays and pass to the system his/her unique ID and the number of days that he/she need to rest. The system check his/her available days and give positive(or negative) answer back, and also inform the employee for the remaining days after detracting the requested days from the available days.`
---
+++ src/App.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("view/app.fxml"));
+        primaryStage.setTitle("Employee Holidays - Sajmir Doko");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("resources/icon.png")));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
